0631e18434ea644ed1a373f0a27c4b5d6ed7d6a1
Diego <diego.gutierrez@agence.cl>
2025-10-24 22:17:41 -0300

    List Chats

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect  } from 'react';
 import { supabase } from '../lib/supabase';
-import ChatRealtime from './ChatRealtime'; 
 import ListaChat from './ListaChats';
 import AdminBookings from "./AdminBookings";
 import { io } from "socket.io-client";
